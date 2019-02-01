1a61a84a7cf3f15759ae8be7d60789d7eda48292
wb-xlb273264 <wb-xlb273264@alibaba-inc.com>
2019-02-01 10:42:10 +0800

    new project

---
+++ src/test/java/com/how2java/tmall_springboot2/TmallSpringboot2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.how2java.tmall_springboot2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TmallSpringboot2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
