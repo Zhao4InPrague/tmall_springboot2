42fb627f407dd389363caeea4f0979f1a703890b
wb-xlb273264 <wb-xlb273264@alibaba-inc.com>
2019-02-14 11:32:44 +0800

    OtherInterceptor 其他拦截器 用于显示购物车总数等

---
+++ src/main/java/com/how2java/tmall/interceptor/OtherInterceptor.java
@@ -0,0 +1,52 @@
+package com.how2java.tmall.interceptor;
+
+import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.OrderItem;
+import com.how2java.tmall.pojo.User;
+import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.service.OrderItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+public class OtherInterceptor implements HandlerInterceptor{
+
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    OrderItemService orderItemService;
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        //这个return有啥关系么??估计要用的话得是true
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        HttpSession session = httpServletRequest.getSession();
+        User user = (User) session.getAttribute("user");
+        int cartTotalItemNumber = 0;
+        if(null != user) {
+            List<OrderItem> orderItems = orderItemService.listByUser(user);
+            for(OrderItem orderItem: orderItems) {
+                cartTotalItemNumber += orderItem.getNumber();
+            }
+        }
+        List<Category> categories = categoryService.list();
+        String contextPath = httpServletRequest.getServletContext().getContextPath();
+        //除了num这俩comtextPath到底是干啥用的??等一下查查
+        httpServletRequest.getServletContext().setAttribute("categories_below_search", categories);
+        session.setAttribute("cartTotalItemNumber", cartTotalItemNumber);
+        httpServletRequest.getServletContext().setAttribute("contextPath", contextPath);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
